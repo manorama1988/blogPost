4804fe31ee97848740a8754c689beb0e7e8d951b
unknown <suni.pal1988@gmail.com>
2021-07-07 03:10:42 +0530

    first commit

--- post.js
+++ post.js
@@ -1,90 +1,41 @@
+const fetchData = async () => {
+    const response = await axios.get('https://peerup-vb-srv.herokuapp.com/Articles ');
+    // console.log(response.data);// show api structure with content
+    for(let newData of response.data){ // showing seprate data with all content 
+        console.log(newData);
+        // console.log(newData._id);// show id 
+        // <h1>${newData._id}</h1>
+        const div = document.createElement('div');
+        div.innerHTML = `
 
-/*
-let msg = document.getElementById("title").innerText; 
-//console.log(msg);
-let msg2 = 'img.png';
-let color = 'red';
-let color2 = 'blue';
-
-let innermsg = document.querySelector(".cardpost-text-inner");
-
-
-let btnPost = document.querySelector('.btn-post');
-btnPost.addEventListener('click', () => {
-  let newArray = [];
-  let array1 = [];
-  array1.push(msg);
-  array1.push(color);
-  console.log(array1);
-
-  let array2 = [];
-  array2.push(msg2);
-  array2.push(color2);
- console.log(array2);
- newArray.push({"key":array1, "value":color
-}, {"key":array2 , "value":color2});
- console.log(newArray);
- for(let i=0; i<newArray.length; i++){
-  
-     console.log(newArray[i]);
-   }
-
-})*/
+        <div class="card-post">
+        <div class="card-post-detail">
+            <div class="card-post-image">
+            <img src="${newData.image.name}" />
+            </div>
+            <div class="card-post-content">
+                <div class="card-post-text">
+                    <h2 class="card-post-title">${newData.title}</h2>
+                    <p class="card-post-date">${newData.published_at}</p>
+                </div>
+                <div class="card-post-description">
+                    <p class="description">${newData.description}</p>
+                </div>
+                <div class="card-post-category">
+                    <button class="category">${newData.category.name}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-let name= "manu";
-let age = "30";
-let a =[];
-a.push(name);
-let b=[];
-b.push(age);
-let array =[];
-//array.push({"key": a , "value":b});
-//console.log(array);
-//array.push({"key": a , "value":b});
-//console.log(array);
-/*
-array.push({"key": age,"value":name});
-console.log(array);
-array.push({"key": age,"value":name});
-console.log(array);*/
+        `;
+            document.querySelector('.autocomplete').appendChild(div); 
+    }
+   
+};
 
-let msgKey = document.getElementById("title").innerText; 
-let msgValue = document.querySelector('.card-post-image').innerText;
-//let image = document.querySelector('.image-text');
+fetchData();
 
-let keyInner = document.querySelector('.card-post-text-inner');
-let valueInner = document.querySelector('.card-post-image-inner');
-//let imgInner = document.querySelector('.image-text-inner');
+const root = document.querySelector('.autocomplete');
 
-
-let array1 = [];
-let array2 = [];
-let newArray = [];
-
-let btnPost = document.querySelector('.btn-post');
-
-//const img = document.querySelector("img"); 
-//img.src = "https://picsum.photos/200/301";
-//document.imgInner.appendChild(img);
-let sectionInner = document.querySelector("#section-inner");
-
-sectionInner.style.display = 'none';
-
-btnPost.addEventListener('click', () => {
-  sectionInner.style.display = 'block';
-
-array1.push(msgKey);
-array2.push(msgValue);
-
-keyInner.textContent = array1;
-
-
-valueInner.textContent = array2;
-
-
-
-
-
-})
-  
